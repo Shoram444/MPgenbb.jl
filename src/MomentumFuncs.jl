53ace15c903210c8fbd099bafc30ed231310f0e8
Maros <petro.maros.afs@gmail.com>
2022-03-02 16:43:50 +0100

    ss

--- src/MomentumFuncs.jl
+++ src/MomentumFuncs.jl
@@ -163,9 +163,9 @@ function get_second_vector(T::Real, MASS::Real, p1::DataFrameRow)
     
     p2Mag = get_pMag(T, MASS)   
     θ2 = acos(p2[3]/p2Mag)
-    ϕ2 = atan(p2[2],p2[1])
+    ϕ2 = atan(p2[2],p2[1])   # computes atan(y/x) while taking care of undefined
 
-    if (p2[1] == NaN)
+    while (p2[1] == NaN)
         get_second_vector(T, MASS, p1)
     end
     
@@ -183,9 +183,9 @@ function get_second_vector(T::Real, MASS::Real, p1::Vector{<:Real})
     
     p2Mag = get_pMag(T, MASS)   
     θ2 = acos(p2[3]/p2Mag)
-    ϕ2 = atan(p2[2],p2[1])
+    ϕ2 = atan(p2[2],p2[1])   # computes atan(y/x) while taking care of undefined
     
-    if (p2[1] == NaN)
+    while (p2[1] == NaN)
         get_second_vector(T, MASS, p1)
     end
 
@@ -203,9 +203,9 @@ function get_second_vector(T::Real, MASS::Real, p1::DataFrameRow, θdif::Real)
     
     p2Mag = get_pMag(T, MASS)
     θ2 = acos(p2[3]/p2Mag)
-    ϕ2 = atan(p2[2],p2[1])
+    ϕ2 = atan(p2[2],p2[1])   # computes atan(y/x) while taking care of undefined
     
-    if (p2[1] == NaN)
+    while (p2[1] == NaN)
         get_second_vector(T, MASS, p1, θdif)
     end
 
@@ -223,10 +223,10 @@ function get_second_vector(T::Real, MASS::Real, p1::Vector{<:Real},  θdif::Real
     
     p2Mag = get_pMag(T, MASS)   
     θ2 = acos(p2[3]/p2Mag)
-    ϕ2 = atan(p2[2],p2[1])  
+    ϕ2 = atan(p2[2],p2[1])   # computes atan(y/x) while taking care of undefined  
 
         
-    if (p2[1] == NaN)
+    while (p2[1] == NaN)
         get_second_vector(T, MASS, p1, θdif)
     end
     
